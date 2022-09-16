26443d9b4de20b6435f030c63f8b14fa6e511a9d
Kromus22 <kromus2@gmail.com>
2022-09-17 01:14:02 +0500

    +js scripts +modal

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 import Swiper from '../lib/swiper-bundle.esm.browser.min.js';
 
+//SimpleBar
+
+new SimpleBar(document.querySelector('.country__list'), {
+  classNames: {
+    scrollbar: 'country__scrollbar',
+    track: 'country__track',
+  }
+});
+
+//Slider
 new Swiper('.goods__block', {
   slidesPerView: 1,
   spaceBetween: 20,
@@ -22,5 +32,94 @@ new Swiper('.goods__block', {
   navigation: {
     prevEl: '.goods__arrow-prev',
     nextEl: '.goods__arrow-next',
+  },
+  preventClicks: true,
+  a11y: false,
+});
+
+// Modal
+
+const productMore = document.querySelectorAll('.product__more');
+const modal = document.querySelector('.modal');
+
+productMore.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    modal.classList.add('modal_open')
+  })
+});
+
+
+modal.addEventListener('click', ({ target }) => {
+  if (target === modal) {
+    modal.classList.remove('modal_open')
+  }
+});
+
+const formPlaceholder = document.querySelectorAll('.form__placeholder');
+const formInput = document.querySelectorAll('.form__input');
+
+formInput.forEach((input, i) => {
+  input.addEventListener('focus', () => {
+    formPlaceholder[i].classList.add('form__placeholder_active')
+  })
+
+  input.addEventListener('blur', () => {
+    if (input.value === '') {
+      formPlaceholder[i].classList.remove('form__placeholder_active')
+    }
+  })
+});
+
+
+//price
+
+const dataCurrency = {};
+
+const formatCurrency = (value, currency) => {
+  return new Intl.NumberFormat('EU', {
+    style: 'currency',
+    currency,
+    maximumFractionDigits: 2,
+  }).format(value)
+}
+
+const showPrice = (currency = 'USD') => {
+  const priceElems = document.querySelectorAll('[data-price]');
+
+  priceElems.forEach(elem => {
+    elem.textContent = formatCurrency(elem.dataset.price * dataCurrency[currency], currency);
+  })
+}
+
+const myHeaders = new Headers();
+myHeaders.append("apikey", "CUeEnklnEJqvy1erhXI24XeGvcavLXpV");
+
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
+
+fetch("https://api.apilayer.com/fixer/latest?&base=USD", requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    Object.assign(dataCurrency, result.rates);
+    showPrice();
+  })
+  .catch(error => console.log('error', error));
+
+// choices 
+
+const countryBtn = document.querySelector('.country__btn');
+const countryWrapper = document.querySelector('.country__wrapper');
+
+countryBtn.addEventListener('click', () => {
+  countryWrapper.classList.toggle('country__wrapper_active')
+});
+
+countryWrapper.addEventListener('click', ({ target }) => {
+  if (target.classList.contains('country__choice')) {
+    countryWrapper.classList.remove('country__wrapper_active');
+    showPrice(target.dataset.currency);
   }
 });
